cb8367548cdd3b0a9ab6c9ae3b99e57dd620c206
arman4558 <armanhossen143964@gmail.com>
2025-12-07 21:31:44 +0600

    Add files via upload

---
+++ largest and smallest number in an array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+    
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int smallest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+        if (arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    cout << "Largest number = " << largest << endl;
+    cout << "Smallest number = " << smallest << endl;
+}
+
